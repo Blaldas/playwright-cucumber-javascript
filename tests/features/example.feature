d7ea66236835cc252f74802b876d2a0a2041c59c
Blaldas <marmingues00@gmail.com>
2025-10-23 21:55:57 +0100

    hooks and steps are working

---
+++ tests/features/example.feature
@@ -0,0 +1,5 @@
+Feature: Product search
+
+    @regression
+    Scenario: Empty search shows validation message
+        When I search for "google"
---
+++ tests/features/step_definitions/home.js
@@ -0,0 +1,19 @@
+import { When } from '@cucumber/cucumber';
+
+// /C:/Users/marmi/Desktop/plawrigt-cucumber-javascript/steps/steps.js
+
+When('I search for {string}', async function (query) {
+    // TODO: perform the search using `query`
+    // Example placeholder: store the query on the world for later assertions
+        console.log(`abhsasbashbhbasahbsahbsbbshbhasashb`);
+         console.log(`abhsasbashbhbasahbsahbsbbshbhasashb`);
+          console.log(`abhsasbashbhbasahbsahbsbbshbhasashb`);
+           console.log(`abhsasbashbhbasahbsahbsbbshbhasashb`);
+            console.log(`abhsasbashbhbasahbsahbsbbshbhasashb`);
+
+
+
+    // If you want to integrate with a page object or browser, do it here:
+    // await this.page.fill('#search', query);
+    // await this.page.click('#search-button');
+});
