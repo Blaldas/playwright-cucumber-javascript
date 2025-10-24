a3fe3114bc99fe8e51fef312061da0f9043c35e9
Blaldas <marmingues00@gmail.com>
2025-10-24 19:56:36 +0100

    improved documentation and fixed argument for first class

--- core/DynamicWorld.js
+++ core/DynamicWorld.js
@@ -1,12 +1,15 @@
 import { setWorldConstructor } from '@cucumber/cucumber';
 import { MonetisWorld } from '../tests/monetis/steps/monetisWorld.js'
 
+/**
+ * @param {{ parameters: { app: any; }; }} options
+ */
 function DynamicWorld(options) {
   const paramApp = options?.parameters?.app;
   const app = paramApp || process.env.APP || 'monetis';
 
   if (app === 'monetis') {
-    return new MonetisWorld(options);
+    return new MonetisWorld();
   }
 }
 
